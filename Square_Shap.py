785ee6823c3d280976e2630004dc2a2a31a2fdd4
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2026-04-06 12:32:46 +0600

    Add turtle graphics code to draw a square

---
+++ Square_Shap.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.Turtle()
+turtle.setup(800,200)
+t.penup()
+t.goto(-300,-50)
+t.pendown()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+turtle.mainloop()
